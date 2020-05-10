2850d6d47bb80caeb5eb7eee18295fa0ebd92a19
William L <w-lindvall@outlook.com>
2020-05-10 00:21:20 -0700

    sketch timeline

--- src/App.js
+++ src/App.js
@@ -1,3 +1,50 @@
 import React from 'react'
 
-export const App = () => <div>hello world</div>
+const TIMELINE_STEP_HEIGHT = 40
+
+const TimelineHourMarkers = ({days = 1}) =>
+  Array.from({length: days}, () =>
+    Array.from({length: 25}, (_, i) => (
+      <div
+        className="timeline__track-division"
+        data-time={i > 12 ? 'night' : 'day'}
+        key={i}
+      >
+        {i !== 0 && <p className="timeline__track-hour-label">{i}</p>}
+      </div>
+    )),
+  )
+
+const Timeline = () => {
+  const TEMP_TIMES = [
+    {label: 'San Fransisco', hour: 10, minutes: 23},
+    {label: 'Stockholm', hour: 21, minutes: 23},
+  ]
+
+  return (
+    <div className="timeline">
+      {TEMP_TIMES.map(({label, hour, minutes}) => (
+        <div
+          className="timeline__track-marker"
+          style={{top: (hour + minutes / 60) * TIMELINE_STEP_HEIGHT}}
+          key={label}
+        >
+          <p className="timeline__track-marker-time">
+            {hour}:{minutes}
+          </p>
+          <p className="timeline__track-marker-label">{label}</p>
+        </div>
+      ))}
+
+      <div className="timeline__track">
+        <TimelineHourMarkers />
+      </div>
+    </div>
+  )
+}
+
+export const App = () => (
+  <main className="main">
+    <Timeline />
+  </main>
+)
