08f5f79b0c5996a933b8a3fbb5bc2242b049a4f4
William L <w-lindvall@outlook.com>
2020-05-11 01:24:48 -0700

    replace existing timezone on add if new timezone exists

--- src/context/timezones/provider.js
+++ src/context/timezones/provider.js
@@ -1,9 +1,34 @@
 import React, {useState} from 'react'
+import moment from 'moment-timezone'
 import {TimezonesContext} from './context'
 
 export const TimezonesContextProvider = ({children}) => {
   const [timezones, setTimezones] = useState([])
-  const addTimezone = timezone => setTimezones(s => [...s, timezone])
+  const addTimezone = timezone => {
+    if (!timezone) return
+
+    const time = moment().tz(timezone.value)
+    const hour24 = time.format('H')
+    const minutes = time.format('mm')
+
+    const existingTimezoneIdx = timezones.findIndex(timezone => {
+      const existingTime = moment().tz(timezone.value)
+      const existingHour24 = existingTime.format('H')
+      const existingMinutes = existingTime.format('mm')
+
+      return existingHour24 === hour24 && existingMinutes === minutes
+    })
+
+    setTimezones(s => {
+      if (existingTimezoneIdx !== -1) {
+        const filteredTimezones = [...timezones]
+        filteredTimezones.splice(existingTimezoneIdx, 1)
+
+        return [...filteredTimezones, timezone]
+      }
+      return [...s, timezone]
+    })
+  }
 
   return (
     <TimezonesContext.Provider value={{timezones, addTimezone}}>
